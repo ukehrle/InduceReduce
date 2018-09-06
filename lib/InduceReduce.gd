807ee4d35d4c33ea03cfa2a4bdafe21ac38020a3
jonathan-144 <42771012+jonathan-144@users.noreply.github.com>
2018-09-06 11:39:32 +0200

    Update InduceReduce.gd
    Adjust the declaration file for changes to come in the implementation file. Most functions have been moved into the record "IndRed"

--- lib/InduceReduce.gd
+++ lib/InduceReduce.gd
@@ -7,139 +7,11 @@
 #
 #############################################################################
 ##
-#F CharacterTableUnger( <G> )
-## 
-## Computes the character table of a finite group using Unger's algorithm
-##
-DeclareGlobalFunction( "CharacterTableUnger" );
-
-#############################################################################
-##
-#F IndRedInit( <G> )
-##
-## Initialization of the elgorithm returns a record which contains all
-## relevant data concerning the group G
-##
-DeclareGlobalFunction( "IndRedInit" );
-
-#############################################################################
-##
-#F IndRedGroupTools( )
-##
-## returns a record with different functions used for the algorithm
-##
-DeclareGlobalFunction( "IndRedGroupTools" );
-
-#############################################################################
-##
-#F IndRedReduceTools( )
-##
-## returns a record with different functions required for lattice reduction
-##
-DeclareGlobalFunction( "IndRedReduceTools" );
-
-#############################################################################
-##
-#F IndRedFindElementary( <GR> )
-##
-## finds the next elementary subgroup and puts it in the component Elementary
-## of the record GR.
-##
-DeclareGlobalFunction( "IndRedFindElementary" );
-
-#############################################################################
-##
-#F IndRedInitElementary( <GR> , <TR> )
-##
-## initialize data concerning the elementary subgroups and exclude its
-## subgroups and their conjugates from the computations yet to come.
-##
-DeclareGlobalFunction( "IndRedInitElementary" );
-
-#############################################################################
-#
-# InduceReduce.gd        The InduceReduce package         Jonathan Gruber
-#
-# Declarations
-#
-# Copyright (C) 2018     Jonathan Gruber
-#
-#############################################################################
-##
 #F CharacterTableUnger( <G> [, <Options>] )
 ## 
 ## Computes the character table of a finite group using Unger's algorithm
 ##
 DeclareGlobalFunction( "CharacterTableUnger" );
-
-#############################################################################
-##
-#F IndRedInit( <G> )
-##
-## Initialization of the elgorithm returns a record which contains all
-## relevant data concerning the group G
-##
-DeclareGlobalFunction( "IndRedInit" );
-
-#############################################################################
-##
-#F IndRedGroupTools( )
-##
-## returns a record with different functions used for the algorithm
-##
-DeclareGlobalFunction( "IndRedGroupTools" );
-
-#############################################################################
-##
-#F IndRedReduceTools( )
-##
-## returns a record with different functions required for lattice reduction
-##
-DeclareGlobalFunction( "IndRedReduceTools" );
-
-#############################################################################
-##
-#F IndRedFindElementary( <GR> , <Opt> )
-##
-## finds the next elementary subgroup and puts it in the component Elementary
-## of the record GR.
-##
-DeclareGlobalFunction( "IndRedFindElementary" );
-
-#############################################################################
-##
-#F IndRedInitElementary( <GR> , <TR> )
-##
-## initialize data concerning the elementary subgroups and exclude its
-## subgroups and their conjugates from the computations yet to come.
-##
-DeclareGlobalFunction( "IndRedInitElementary" );
-
-#############################################################################
-##
-#F IndRedInduceCyc( <GR> )
-##
-## induce characters from all cyclic subgroups
-##
-DeclareGlobalFunction( "IndRedInduceCyc" );
-
-#############################################################################
-##
-#F IndRedInduce( <GR> )
-##
-## Induce all irreducible characters of the elementary subgroup in 
-## GR.Elementary to the group GR.G and add them to GR.B
-##
-DeclareGlobalFunction( "IndRedInduce" );
-
-#############################################################################
-#
-# IndRedReduce( <GR>, ><RedTR> )
-#
-# reduce the induced characters by the irreducibles found so far and do LLL
-# lattice reduction
-#
-DeclareGlobalFunction( "IndRedReduce" );
 
 #############################################################################
 ##
@@ -152,12 +24,11 @@ DeclareGlobalFunction( "InduceReduce" );
 
 #############################################################################
 ##
-#F IndRedtinI( <GR> )
+#V IndRed
 ##
-## adjusts the signs of the characters and permutes them to the right
-## ordering of conjugacy classes
+## a record containing subfunctions of CharacterTableUnger and InduceReduce
 ##
-DeclareGlobalFunction( "IndRedtinI" );
+DeclareGlobalVariable( "IndRed" );
 
 #############################################################################
 ##
